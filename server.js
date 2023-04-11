89c3638af9f71b7c08066b7e770fc2bfbb4daa2e
nonchalamment <nonchalamment@gmail.com>
2023-04-11 14:46:32 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+// import modules
+
+import express from 'express'
+
+// create Express app
+
+const app = express()
+
+// configure the app (app.set)
+
+
+
+// mount Middleware (app.use)
+
+
+
+// Mount routes
+
+app.get('/', function(req, res) {
+  res.send('<h1>hello, friend</h1>')
+})
+
+// tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
