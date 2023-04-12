f29dd7c25add8eade5a637b5f251a073722e16fc
nonchalamment <nonchalamment@gmail.com>
2023-04-11 20:57:24 -0400

    edit students-data.js to include more data

--- data/students-data.js
+++ data/students-data.js
@@ -1,9 +1,9 @@
 const students = [
-  {name: 'Greta', passed: true, _id: 593023},
-  {name: 'Albert', passed: false, _id: 401938},
-  {name: 'Veronica', passed: true, _id: 145049},
-  {name: 'Harley', passed: false, _id: 105035},
-  {name: 'Paula', passed: true, _id: 514927},
+  {name: 'Greta', class: 'Math', passed: true, _id: 593023},
+  {name: 'Albert', class: 'History', passed: false, _id: 401938},
+  {name: 'Veronica', class: 'Math', passed: true, _id: 145049},
+  {name: 'Harley', class: 'English', passed: false, _id: 105035},
+  {name: 'Paula', class: 'English', passed: true, _id: 514927},
 ]
 
 export {
